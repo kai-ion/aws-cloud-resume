286ea664bdc81d1b94028eadc89027200ea51e23
kaiion <kaiion0509@gmail.com>
2024-04-06 21:30:44 -0400

    added script to display visitor counts from lambda function on aws

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,13 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch(
+        "https://ql64kmu52k3ergayeewlsz4akm0nhvdv.lambda-url.us-west-1.on.aws/"
+    );
+    let data = await response.json();
+    counter.innerHTML = `👀 Views: ${data}`;
+}
+updateCounter();
